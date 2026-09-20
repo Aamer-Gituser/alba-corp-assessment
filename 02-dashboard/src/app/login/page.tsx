46c94371964eee9854062c8c1f8272fa67d88a2d
Aamer-Gituser <khanaamer.170304@gmail.com>
2026-09-20 12:25:51 +0530

    fix: update default demo email to use Gmail test account

--- 02-dashboard/src/app/login/page.tsx
+++ 02-dashboard/src/app/login/page.tsx
@@ -5,7 +5,7 @@ import { signIn, signUp, type AuthState } from './actions'
 
 const EMPTY: AuthState = { error: null, notice: null }
 
-const DEMO_EMAIL = process.env.NEXT_PUBLIC_DEMO_EMAIL ?? 'demo@forecourt.test'
+const DEMO_EMAIL = process.env.NEXT_PUBLIC_DEMO_EMAIL ?? 'appflow.qa01@gmail.com'
 const DEMO_PASSWORD = process.env.NEXT_PUBLIC_DEMO_PASSWORD ?? 'forecourt-demo'
 
 export default function LoginPage() {
